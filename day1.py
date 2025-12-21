b662e932249620a4ea2d2448d641b54059f88010
rjuchnicki <1916201+rjuchnicki@users.noreply.github.com>
2025-12-21 10:07:42 -0800

    Day 1 Part 2

--- day1.py
+++ day1.py
@@ -3,9 +3,24 @@ def part1(instructions: list[int]):
     pos = 50
     for i in instructions:
         pos = (pos + i) % 100
-
         if pos == 0:
             count += 1
+
+    return count
+
+
+def part2(instructions: list[int]):
+    count = 0
+    pos = 50
+    for i in instructions:
+        full_rotations, rem = divmod(abs(i), 100)
+        count += full_rotations
+
+        next_pos = pos + rem * (1 if i > 0 else -1)
+        if pos != 0 and (i < 0 and next_pos <= 0) or (i > 0 and next_pos > 99):
+            count += 1
+
+        pos = next_pos % 100
 
     return count
 
@@ -19,3 +34,4 @@ if __name__ == "__main__":
     ]
 
     print(part1(instructions))
+    print(part2(instructions))
