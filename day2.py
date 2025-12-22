ffaf3ac2cf4995383a2300315de3782905ebad33
rjuchnicki <1916201+rjuchnicki@users.noreply.github.com>
2025-12-21 22:32:47 -0800

    Day 2 Part 2

--- day2.py
+++ day2.py
@@ -12,6 +12,33 @@ def part1(id_ranges: list[list[int]]):
     return total
 
 
+def part2(id_ranges: list[list[int]]):
+    total = 0
+    for start, end in id_ranges:
+        while start <= end:
+            num_str = str(start)
+            n = len(num_str)
+
+            for i in range(1, n // 2 + 1):
+                seq = num_str[:i]
+                j = i
+
+                made_with_repeated = True
+                while j < n:
+                    if num_str[j : j + i] != seq:
+                        made_with_repeated = False
+                        break
+                    j += i
+
+                if made_with_repeated:
+                    total += start
+                    break
+
+            start += 1
+
+    return total
+
+
 if __name__ == "__main__":
     with open("data/day2.txt", "r") as f:
         id_ranges = [
@@ -19,3 +46,4 @@ if __name__ == "__main__":
         ]
 
     print(part1(id_ranges))
+    print(part2(id_ranges))
