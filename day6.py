213e79ff3e1e91d2555f96cf5bf750b2c7996d1d
rjuchnicki <1916201+rjuchnicki@users.noreply.github.com>
2025-12-28 20:43:18 -0800

    Day 6 Part 2

--- day6.py
+++ day6.py
@@ -14,11 +14,42 @@ def part1(operands: list[list[int]], operators: list[str]):
     return total
 
 
+def compute_result(numbers: list[str], operator: str) -> int:
+    operands = [
+        int("".join(n[i] for n in numbers)) for i in range(len(numbers[0]))
+    ]
+    return math.prod(operands) if operator == "*" else sum(operands)
+
+
+def part2(operands_str: list[str], operators: list[str]):
+    total = 0
+    operator_index = len(operators) - 1
+    numbers = [""] * len(operands_str)
+
+    for i in range(len(operands_str[0]) - 1, -2, -1):
+        if i == -1:
+            total += compute_result(numbers, operators[operator_index])
+            break
+        elif all(s[i] == " " for s in operands_str):
+            total += compute_result(numbers, operators[operator_index])
+            numbers = [""] * len(operands_str)
+            operator_index -= 1
+            continue
+
+        numbers = [
+            operands_str[j][i] + numbers[j] for j in range(len(operands_str))
+        ]
+
+    return total
+
+
 if __name__ == "__main__":
     with open("data/day6.txt", "r") as f:
         input = f.readlines()
 
     operands = [[int(n) for n in i.split()] for i in input[:-1]]
+    operands_str = [i.replace("\n", "") for i in input[:-1]]
     operators = input[-1].split()
 
     print(part1(operands, operators))
+    print(part2(operands_str, operators))
